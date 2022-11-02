bc591fa64493631eeb8b7c748b0b834c8b12dbc7
ntduc-let <savatar2204@gmail.com>
2022-11-03 03:50:35 +0700

    update

---
+++ MusicPlayerUtils/src/main/java/com/ntduc/musicplayerutils/player/MusicPlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.ntduc.musicplayerutils.player
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ntduc.musicplayerutils.R
+
+class MusicPlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_player)
+    }
+}
